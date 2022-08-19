d057462d1edec02d871ac5270bd8fe84d43521c4
wdssmq <wdssmq@qq.com>
2022-08-19 15:04:47 +0800

    fix. fastify listen method for new ver

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ export async function boot(config) {
   try {
     const resolvedPort = await getPort({ host: (config.host ?? '127.0.0.0'), port: [config.port, ...makeRange(8081, 9000)] })
     const server = await init(config)
-    await server.listen(resolvedPort, config.host)
+    await server.listen({ port: resolvedPort, host: config.host })
     if (config.onListen) config.onListen(server)
   } catch (err) {
     console.error(err)
